9fecf85fa08a7fb2f31051aca62544d3f1250c32
Matthew Clemens <matt.d.clemens@gmail.com>
2012-11-04 11:00:12 -0800

    renamed the basic resizable array.
    the renaming was to avoid conflicts going forward between the complex flex array and the simple resizable array, now called dyna.

--- src/dynaArray/dyna.h
+++ src/dynaArray/dyna.h
@@ -1,5 +1,5 @@
-#ifndef _flex_h
-#define _flex_h
+#ifndef _dyna_h
+#define _dyna_h
 
 
 typedef enum  {
@@ -16,29 +16,29 @@ typedef unsigned int index_t;
 
 typedef void (*free_func_t)(data_t ptr);
 typedef int (*cmp_func_t)(data_t a, data_t b);
-typedef struct flex_array * flex_t;
+typedef struct dyna_array * dyna_t;
 
 /* Container altering functions.*/
-flex_t flex_init(
+dyna_t dyna_init(
         index_t inital_size);
 
-DSTATUS flex_destroy(
-        flex_t flex);
+DSTATUS dyna_destroy(
+        dyna_t dyna);
 
-DSTATUS flex_change_free(flex_t flex,
+DSTATUS dyna_change_free(dyna_t dyna,
         free_func_t func);
 
-DSTATUS flex_change_cmp(flex_t flex,
+DSTATUS dyna_change_cmp(dyna_t dyna,
         cmp_func_t func);
 
 /* Element manipulation functions.*/
-DSTATUS flex_insert(flex_t flex,
+DSTATUS dyna_insert(dyna_t dyna,
         index_t requested_index, data_t user_data, DSTATUS modifier);
 
-DSTATUS flex_delete(flex_t flex,
+DSTATUS dyna_delete(dyna_t dyna,
         index_t requested_index);
 
-data_t flex_get(flex_t flex, 
+data_t dyna_get(dyna_t dyna, 
         index_t requested_index);
 
 #endif
